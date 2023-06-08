27afab0359c09dd348de5fc181e02f483ff7bde0
juanmllanos <juan_m@hotmail.com.ar>
2023-06-07 22:53:55 -0300

    Componente Titulo terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import React from 'react'
 import { Footer } from './components/Footer'
+import { Titulo } from './components/Titulo'
 
 function App() {
 
   return (
     <>
+      <Titulo></Titulo>
       <Footer></Footer>
     </>
   )
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const Titulo = () => {
+	return(
+		<Container>
+			<h1 className='display-1 text-center'>Noticias</h1>
+			<hr />
+		</Container>
+	)
+}
+
+export { Titulo }
